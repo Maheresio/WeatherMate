e0b7bb9d5e2397601207663e5e75c50a68246dc0
Maheresio <zamalek.545r5@gmail.com>
2025-02-13 06:20:59 +0200

    using adaptive layout in the login view

--- lib/features/auth/presentation/view/login_view.dart
+++ lib/features/auth/presentation/view/login_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_mate/features/auth/presentation/view/desktop_login_view_body.dart';
 import 'package:weather_mate/features/auth/presentation/view/mobile_login_view_body.dart';
 import 'package:weather_mate/features/auth/presentation/view/tablet_login_view_body.dart';
+import 'package:weather_mate/features/auth/presentation/view/widgets/adaptive_layout.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({super.key});
@@ -9,18 +10,10 @@ class LoginView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: LayoutBuilder(
-          builder: (context, constraints) {
-            if (constraints.maxWidth < 600) {
-              return MobileLoginViewBody();
-            } else if (constraints.maxWidth < 1200) {
-              return TabletLoginViewBody();
-            } else {
-              return DesktopLoginViewBody();
-            }
-          },
-        ),
+      body: AdaptiveLayout(
+        mobileLayout: (context) => MobileLoginViewBody(),
+        tabletLayout: (context) => TabletLoginViewBody(),
+        desktopLayout: (context) => DesktopLoginViewBody(),
       ),
     );
   }
