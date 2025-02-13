394351d2056406cece8475701d42a1dc1b96afde
Maheresio <zamalek.545r5@gmail.com>
2025-02-14 00:05:36 +0200

    refactor firebase init method to ignore linux

--- lib/core/helpers/firebase_init.dart
+++ lib/core/helpers/firebase_init.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
 import 'package:firebase_core/firebase_core.dart';
 
 import '../../firebase_options.dart';
 
 Future<void> firebaseInit() async {
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  if (!Platform.isLinux) {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
 }
