6460d3e2ac810cad3b4968ab9477489212252d0e
Maheresio <zamalek.545r5@gmail.com>
2025-02-14 03:30:04 +0200

    adding the auth repository and the implementation and refactor it

--- lib/features/auth/data/auth_repository.dart
+++
@@ -1,6 +0,0 @@
-abstract class AuthRepository {
-  Future<void> loginWithEmailAndPassword({required email, required password});
-  Future<void> registerWithEmailAndPassword(
-      {required email, required password});
-  Future<void> signOut();
-}
--- lib/features/auth/data/auth_repository_impl.dart
+++ lib/features/auth/data/auth_repository_impl.dart
@@ -1,19 +1,92 @@
-import 'package:weather_mate/features/auth/data/auth_repository.dart';
+import 'package:dartz/dartz.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:weather_mate/core/error/failure.dart';
+import 'package:weather_mate/features/auth/domain/entity/user_entity.dart';
+import 'package:weather_mate/features/auth/domain/repository/auth_repository.dart';
+
+import 'firebase_auth_data_source.dart';
 
 class AuthRepositoryImpl implements AuthRepository {
+  final FirebaseAuthDataSource firebaseAuthDataSource;
+  final SharedPreferences prefs;
+
+  AuthRepositoryImpl({
+    required this.firebaseAuthDataSource,
+    required this.prefs,
+  });
+
   @override
-  Future<void> loginWithEmailAndPassword({required email, required password}) {
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity>> loginWithEmailAndPassword(
+      {required String email, required String password}) async {
+    try {
+      final user = await firebaseAuthDataSource.signInWithEmailAndPassword(
+          email: email, password: password);
+
+      await _cacheUser(user);
+
+      return Right(user);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
   }
 
   @override
-  Future<void> registerWithEmailAndPassword(
-      {required email, required password}) {
-    throw UnimplementedError();
+  Future<Either<Failure, UserEntity?>> getCachedUser() async {
+    try {
+      final email = prefs.getString('userEmail');
+      final idToken = prefs.getString('idToken');
+      if (email != null && idToken != null) {
+        return Right(UserEntity(email: email, idToken: idToken));
+      }
+      return const Right(null);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
   }
 
   @override
-  Future<void> signOut() {
-    throw UnimplementedError();
+  Future<Either<Failure, String>> refreshToken() async {
+    try {
+      final newToken = await firebaseAuthDataSource.refreshToken();
+      await prefs.setString('idToken', newToken);
+      return Right(newToken);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  @override
+  Future<Either<Failure, void>> signOut() async {
+    try {
+      await firebaseAuthDataSource.signOut();
+      await _clearCache();
+      return Right(null);
+    } catch (e) {
+      return Left(Failure(e.toString()));
+    }
+  }
+
+  Future<void> _cacheUser(UserEntity user) async {
+    await prefs.setString('userEmail', user.email!);
+    await prefs.setString('idToken', user.idToken!);
+  }
+
+  Future<void> _clearCache() async {
+    await prefs.remove('userEmail');
+    await prefs.remove('idToken');
+  }
+  
+  @override
+  Future<Either<Failure, UserEntity>> registerWithEmailAndPassword({required String email, required String password}) async{
+     try {
+      final user = await firebaseAuthDataSource.registerWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+      await _cacheUser(user);
+      return Right(user);
+    } catch (e) {
+      return Left(Failure( e.toString()));
+    }
   }
 }
