8e8903ab685ac9f04dcccaf8793be3edd9425a31
Maheresio <zamalek.545r5@gmail.com>
2025-02-14 08:46:41 +0200

    refactor login form to use controllers and add validation and bloc consumer

--- lib/features/auth/presentation/view/login/widgets/login_fields.dart
+++ lib/features/auth/presentation/view/login/widgets/login_fields.dart
@@ -1,25 +1,33 @@
 import 'package:flutter/material.dart';
 
+import '../../../../../../core/helpers/validation_methods.dart';
 import '../../../../../../core/utils/app_strings.dart';
 import '../../components/styled_text_form_field.dart';
 
 class LoginFields extends StatelessWidget {
-  const LoginFields({super.key});
+  const LoginFields(
+      {super.key,
+      required this.emailController,
+      required this.passwordController});
 
+  final TextEditingController emailController;
+  final TextEditingController passwordController;
   @override
   Widget build(BuildContext context) {
     return Column(
       spacing: 20,
       children: [
         StyledTextFormField(
-          labelText: AppStrings.username,
-          suffixIcon: Icons.person,
-          controller: TextEditingController(),
+          labelText: AppStrings.email,
+          suffixIcon: Icons.email_rounded,
+          controller: emailController,
+          validator: (value) => emailValidator(value),
         ),
         StyledTextFormField(
           labelText: AppStrings.password,
           suffixIcon: Icons.lock,
-          controller: TextEditingController(),
+          controller: passwordController,
+          validator: (value) => passwordValidator(value),
         ),
       ],
     );
--- lib/features/auth/presentation/view/login/widgets/login_form.dart
+++ lib/features/auth/presentation/view/login/widgets/login_form.dart
@@ -1,37 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_mate/features/auth/presentation/controller/auth_cubit.dart';
 
+import '../../../../../../core/utils/app_colors.dart';
 import '../../../../../../core/utils/app_strings.dart';
 import '../../../../../../core/utils/app_styles.dart';
 import '../../components/auth_button.dart';
 import 'login_fields.dart';
 import '../../components/social_auth_section.dart';
 
-class LoginForm extends StatelessWidget {
+class LoginForm extends StatefulWidget {
   const LoginForm({
     super.key,
   });
 
   @override
+  State<LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      spacing: 10,
-      children: [
-        Text(
-          AppStrings.login,
-          style: AppStyles.textStyleBold55(context),
-        ),
-        LoginFields(),
-        Text(
-          AppStrings.forgotPassword,
-          style: AppStyles.textStyleNormal20(context),
-        ),
-        AuthButton(
-          buttonText: AppStrings.login,
-          onPressed: () {},
-        ),
-        SocialAuthSection(),
-      ],
+    return Form(
+      key: _formKey,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        spacing: 10,
+        children: [
+          Text(
+            AppStrings.login,
+            style: AppStyles.textStyleBold55(context),
+          ),
+          LoginFields(
+            emailController: emailController,
+            passwordController: passwordController,
+          ),
+          Text(
+            AppStrings.forgotPassword,
+            style: AppStyles.textStyleNormal20(context),
+          ),
+          BlocConsumer<AuthCubit, AuthState>(
+            listener: (context, state) {
+              if (state is AuthFailure) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    backgroundColor: Colors.redAccent,
+                    content: Text(state.message),
+                  ),
+                );
+              }
+
+              if (state is AuthSuccess) {}
+            },
+            builder: (context, state) {
+              if (state is AuthLoading) {
+                return Center(
+                  child: const CircularProgressIndicator(
+                    color: AppColors.primaryColor,
+                  ),
+                );
+              }
+
+              return AuthButton(
+                buttonText: AppStrings.login,
+                onPressed: () {
+                  if (_formKey.currentState!.validate()) {
+                    context.read<AuthCubit>().loginWithEmailAndPassword(
+                          email: emailController.text,
+                          password: passwordController.text,
+                        );
+                  }
+                },
+              );
+            },
+          ),
+          SocialAuthSection(),
+        ],
+      ),
     );
   }
 }
